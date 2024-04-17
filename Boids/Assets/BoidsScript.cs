2b1d3b57b5a40bcb5fe64be303295febf2287d46
Verc1n <136546706+Verc1n@users.noreply.github.com>
2024-04-17 16:25:26 +1000

    Uh

---
+++ Boids/Assets/BoidsScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public Vector2 Position;
+    public Vector2 Velocity;
+    public Vector2 Rotation;
+    public Vector2 Acceleration;
+    
+}
